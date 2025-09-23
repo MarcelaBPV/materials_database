5833fbb8b8af34f57b9b276e6563852b0b74c892
MarcelaBPVeiga <167220485+MarcelaBPV@users.noreply.github.com>
2025-09-23 11:41:32 -0300

    app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 from supabase import create_client, Client
 
 # Conexão com Supabase usando secrets
+
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-st.title("📊 Materials Database")
+
+st.title("📊 Material Surface Characterization Platform")
 st.write("Exemplo de conexão com banco de dados no Supabase.")
 
 # Exemplo: listar dados da tabela "samples"
